819db868b581aae98721e7c92eb4ca5a14f2f05a
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-08-26 13:01:56 +0530

    Episode 6

---
+++ 06-Join Leave Logs/commands/info/ping.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name : 'ping',
+    category : 'info',
+    description : 'Returns latency and API ping',
+    run : async(client, message, args) => {
+        const msg = await message.channel.send(`🏓 Pinging...`)
+        const embed = new MessageEmbed()
+            .setTitle('Pong!')
+            .setDescription(`WebSocket ping is ${client.ws.ping}MS\nLatency is ${Math.floor(msg.createdAt - message.createdAt)}MS!`)
+            await message.channel.send({ embeds: [embed] })
+            msg.delete()
+    }
+}
