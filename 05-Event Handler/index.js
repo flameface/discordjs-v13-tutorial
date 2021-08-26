349196177bbcf21b3e610cefb178deec2e3501c1
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-08-26 12:42:17 +0530

    Episode 5

---
+++ 05-Event Handler/index.js
@@ -0,0 +1,37 @@
+const {Collection, Client, Discord} = require('discord.js')
+const config = require('./config.json');
+const fs = require('fs');
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+    ],
+});
+
+module.exports = client;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} is ready`)
+})
+
+client.login(config.token)
