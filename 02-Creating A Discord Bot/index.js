31cf337baba826c4ea2a110d62a4e2544c6675fa
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-08-26 12:27:04 +0530

    Episode 2

--- 02-Creating A Discord Bot/index.js
+++ 02-Creating A Discord Bot/index.js
@@ -1,8 +1,7 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-  console.log('Discord Bot Is Ready');
-});
-
-client.login('your-token-here')
+const { Discord, Client, Collection, MessageEmbed, Intents } = require('discord.js');
+const client = new Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_INVITES']});
+const config = require('./config.json');
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready`)
+})
+client.login(config.token)
