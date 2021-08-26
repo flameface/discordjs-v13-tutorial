27dd161a231897957cd13bb4fddf8975c77e7493
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-08-26 13:12:35 +0530

    Episode 8

---
+++ 08-Auto Role/events/autorole.js
@@ -0,0 +1,9 @@
+const client = require('../index.js')
+const db = require("quick.db")
+
+client.on('guildMemberAdd', async(member) => {
+  const role = await db.has(`autorole-${member.guild.id}`);
+  if(role === true) {
+    member.roles.add(await db.get(`autorole-${member.guild.id}`))
+  }
+})
