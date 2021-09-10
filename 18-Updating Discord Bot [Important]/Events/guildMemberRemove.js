42fe064eee081138124c001c97169f15e0238197
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-10 20:00:21 +0530

    Update guildMemberRemove.js

--- 18-Updating Discord Bot [Important]/Events/guildMemberRemove.js
+++ 18-Updating Discord Bot [Important]/Events/guildMemberRemove.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 client.on('guildMemberRemove', async(member) => {
   const Channel = member.guild.channels.cache.get('channel-id');
   const embed = new MessageEmbed()
-  .setColor("GREEN")
+  .setColor("RED")
   .setTitle("Member Leave")
   .setDescription(`**${member.displayName}** Leave ${member.guild.name}, We Have Now ${member.guild.memberCount} Members`);
   Channel.send({ embeds: [embed] });
