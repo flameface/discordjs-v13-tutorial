6c6f5edb5c672f27df116d554101e37f8186253a
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-14 14:47:37 +0530

    Update kick.js

--- 18-Updating Discord Bot [Important]/Commands/Moderation/kick.js
+++ 18-Updating Discord Bot [Important]/Commands/Moderation/kick.js
@@ -6,18 +6,18 @@ module.exports = {
       description: 'Kicks a member from your server.',
       clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS', 'KICK_MEMBERS'],
       userPerms: ['KICK_MEMBERS'],
-      run: async(client, message, args) => {
+      run: async(client, message, args, prefix) => {
           if(!message.content.startsWith(prefix)) return;
           try{
-          const member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
+          const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first();
           if (!member)
-          return this.sendErrorMessage(message, 0, `${client.emoji.fail} Please mention a user or provide a valid user ID`);
+          return message.channel.send(`${client.emoji.fail} Please mention a user or provide a valid user ID`);
           if (member === message.member)
-          return this.sendErrorMessage(message, 0, `${client.emoji.fail} You cannot kick yourself`);
+          return message.channel.send(`${client.emoji.fail} You cannot kick yourself`);
           if (member.roles.highest.position >= message.member.roles.highest.position)
-          return this.sendErrorMessage(message, 0, `${client.emoji.fail} You cannot kick someone with an equal or higher role`);
+          return message.channel.send(`${client.emoji.fail} You cannot kick someone with an equal or higher role`);
           if (!member.kickable)
-          return this.sendErrorMessage(message, 0, `${client.emoji.fail} Provided member is not kickable`);
+          return message.channel.send(`${client.emoji.fail} Provided member is not kickable`);
           let reason = args.slice(1).join(' ');
           if (!reason) reason = '`None`';
           if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
@@ -25,15 +25,16 @@ module.exports = {
           const embed = new MessageEmbed()
           .setTitle('Kick Member')
           .setDescription(`${client.emoji.success} ${member} was successfully kicked.`)
-          .addField('Moderator', message.member, true)
-          .addField('Member', member, true)
-          .addField('Reason', reason)
+          .addField('Moderator', `${message.member || undefined}`, true)
+          .addField('Member', `${member || undefined}`, true)
+          .addField('Reason', `${reason || undefined}`)
           .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
           .setTimestamp()
-          .setColor("GREEN");
-          message.channel.send(embed);
-          message.client.logger.info(`${message.guild.name}: ${message.author.tag} kicked ${member.user.tag}`);
-      
+          .setColor("GREEN")
+          message.channel.send({ embeds: [embed] })
+          message.author.send(`${message.guild.name}: ${message.author.tag} kicked ${member.user.tag} for reason: ${reason}`)
+              
+
       //ERROR CATCH
       } catch (err) {
       const errorEmbed = new MessageEmbed()
@@ -41,7 +42,7 @@ module.exports = {
       .setDescription(`${client.emoji.fail} ${err.message}`)
       .setColor("RED")
       .setFooter("message will be deleted after 10 seconds");
-      message.channel.send({embeds: [errorEmbed] }).then(e => {
+      message.channel.send({ embeds: [errorEmbed] }).then(e => {
         setTimeout(() => e.delete(), 10000);
       });
     }
