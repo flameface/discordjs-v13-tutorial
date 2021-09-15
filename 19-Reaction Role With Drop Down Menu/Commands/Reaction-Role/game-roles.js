13314b24ad8dee7f22c998080c757088763cfda8
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-15 15:48:03 +0530

    Episode 19

--- 19-Reaction Role With Drop Down Menu/Commands/Reaction-Role/game-roles.js
+++ 19-Reaction Role With Drop Down Menu/Commands/Reaction-Role/game-roles.js
@@ -1,34 +1,33 @@
-const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require('discord.js');
+const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name: "gamerole",
-    description: "get gaming console role",
-    ClientPerms: ["ADMINISTRATOR"],
-    UserPerms: ["ADMINISTRATOR"],
-    run: async(client, message, args, prefix) => {
-        if(!message.content.startsWith(prefix)) return;
-        const embed = new MessageEmbed()
-        .setTitle("Gaming Console Roles")
-        .setDescription("Get And Remove Roles By Clicking On Options Given In Menu, After Reclicking The Role Will Be Removed, The Bot Will Send DM Message That You Have Added Or Removed Role")
-		 const row = new MessageActionRow()
-			.addComponents(
-				new MessageSelectMenu()
-					.setCustomId('gamerole')
-					.setPlaceholder('Select To Get Roles')
-					.addOptions([
-						{
-							label: 'XBOX Role',
-							description: 'Click To Get Or Remove Male Role',
-							value: 'xbox',
-						},
-						{
-							label: 'PlayStation Role',
-							description: 'Click To Get Or Remove Playstation Role',
-							value: 'playstation',
-						},
-					]),
-			);
-
-		await message.channel.send({ embeds: [embed], components: [row] });
-	  }
+  name: 'gamerole',
+  description: "get gaming console roles",
+  userPerms: ["ADMINISTRATOR"],
+  run: async(client, message, args, prefix) => {
+    if(!message.content.startsWith(prefix)) return;
+    const embed = new MessageEmbed()
+    .setTitle("Gaming Console Roles")
+    .setDescription("Get Or Remove Roles By Clicking On Options Given In menu, After Reclicking The Role Will Be Removed, The Bot Will Send DM Message That You Have Added/Removed Roles")
+    
+    const row = new MessageActionRow()
+    .addComponents(
+      new MessageSelectMenu()
+      .setCustomId('gamerole')
+      .setPlaceholder('Select To Add/Remove Roles')
+      .addOptions([
+        {
+          label: 'XBOX Role',
+          description: "Click To Add/Remove XBOX Roles",
+          value: 'xbox',
+        },
+        {
+          label: 'PlayStation Role',
+          description: "Click To Add/Remove PlayStation Roles",
+          value: 'playstation',
+        },
+        ]),
+      );
+      await message.channel.send({ embeds: [embed], components: [row] });
   }
+}
