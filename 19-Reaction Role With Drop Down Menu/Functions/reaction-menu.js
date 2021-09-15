6fba8a1c6b94867feb828e7db406858522e7eeb9
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-15 15:47:32 +0530

    Episode 19

--- 19-Reaction Role With Drop Down Menu/Functions/reaction-menu.js
+++ 19-Reaction Role With Drop Down Menu/Functions/reaction-menu.js
@@ -1,31 +1,31 @@
 const client = require("../index.js")
 const wait = require('util').promisify(setTimeout);
-
 client.on('interactionCreate', async interaction => {
-   const member = await interaction.message.guild.members.fetch({ user: interaction.user.id, force: true})
-	if (!interaction.isSelectMenu()) return;
-  
+  const member = await interaction.message.guild.members.fetch({
+    user: interaction.user.id,
+    force: true 
+  })
+  if(!interaction.isSelectMenu()) return;
   //XBOX ROLE
-	if (interaction.values == 'xbox') {
-       await interaction.deferUpdate();
-       if(!member.roles.cache.has('869767240537485312')) {
-            await member.roles.add('869767240537485312')
-            return interaction.member.send(`${client.emoji.success} We Have Added XBOX Role Successfully`, true)
-       } else if(member.roles.cache.has('869767240537485312')) {
-            await member.roles.remove('869767240537485312')
-            return interaction.member.send(`${client.emoji.fail} We Have Removed XBOX Role`, true)
-       }
-	}
-  
-  //PLAYSTATION ROLE
-	if (interaction.values == 'playstation') {
-       await interaction.deferUpdate();
-       if(!member.roles.cache.has('869767331797151754')) {
-            await member.roles.add('869767331797151754')
-            return interaction.member.send(`${success} We Have Added Playstation Role Successfully`, true)
-       } else if(member.roles.cache.has('869767331797151754')) {
-            await member.roles.remove('869767331797151754')
-            return interaction.member.send(`${fail} We Have Removed Playstation Role`, true)
-       }
+  if(interaction.values == 'xbox') {
+    await interaction.deferUpdate();
+    if(!member.roles.cache.has("887611778694004756")) {
+      await member.roles.add('887611778694004756')
+      return interaction.member.send(`${client.emoji.success} We Have Added XBOX Role`)
+    } else if(member.roles.cache.has("887611778694004756")) {
+      await member.roles.remove('887611778694004756')
+      return interaction.member.send(`${client.emoji.fail} We Have Remove XBOX Role`)
     }
-});
+  }
+  //PlayStation ROLE
+  if(interaction.values == 'playstation') {
+    await interaction.deferUpdate();
+    if(!member.roles.cache.has("887611916472696844")) {
+      await member.roles.add('887611916472696844')
+      return interaction.member.send(`${client.emoji.success} We Have Added PlayStation Role`)
+    } else if(member.roles.cache.has("887611916472696844")) {
+      await member.roles.remove('887611916472696844')
+      return interaction.member.send(`${client.emoji.fail} We Have Remove PlayStation Role`)
+    }
+  }
+})
