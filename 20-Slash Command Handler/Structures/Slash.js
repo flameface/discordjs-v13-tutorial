4227e394c24d868113b387cecbc221d6862e8b51
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-22 16:51:31 +0530

    Update Slash.js

--- 20-Slash Command Handler/Structures/Slash.js
+++ 20-Slash Command Handler/Structures/Slash.js
@@ -1,25 +1,22 @@
 let slash = []
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
-
-let table = new ascii("Slash commands");
+let table = new ascii("Slash Commands");
+table.setHeading('Slash Command', ' Load status');
 module.exports = (client) => {
     readdirSync("./SlashCommands/").forEach(dir => {
         const commands = readdirSync(`./SlashCommands/${dir}/`).filter(file => file.endsWith(".js"));
-    
         for (let file of commands) {
             let pull = require(`../SlashCommands/${dir}/${file}`);
-    
             if (pull.name) {
                 client.slash.set(pull.name, pull);
                 slash.push(pull);
-                table.addRow(file, '✅');
+                table.addRow(file, '✔️ -> Loaded Slash Command');
             } else {
                 table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
                 continue;
-            }
-    
-            }
+             }
+          }
     });
     console.log(table.toString());
 client.on("ready",async ()=> {
