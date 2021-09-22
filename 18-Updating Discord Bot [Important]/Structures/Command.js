a386708ab7cc807563191c24ef0046e5d84bc53e
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-22 16:01:18 +0530

    Update Command.js

--- 18-Updating Discord Bot [Important]/Structures/Command.js
+++ 18-Updating Discord Bot [Important]/Structures/Command.js
@@ -1,15 +1,23 @@
 const {readdirSync} = require('fs');
+const ascii = require('ascii-table');
+let table = new ascii("Commands");
+table.setHeading('Command', ' Load status');
 
 module.exports = (client) => {
-  client.logger.log("=-=-=-=-=-=-=- Loading command(s) -=-=-=-=-=-=-=")
-  readdirSync('./Commands').forEach(dir => {
-    const commands = readdirSync(`./Commands/${dir}/`).filter(file => file.endsWith('.js'));
-    for(let file of commands){
-      let pull = require(`../Commands/${dir}/${file}`);
-      if(pull.name){
-        client.commands.set(pull.name, pull);
-        client.logger.log(`Loading Command: ${pull.name}`)
-      } if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
-    }
-  })
+    client.logger.ready("=-=-=-=-=-=-=- Loading command(s) -=-=-=-=-=-=-=");
+    readdirSync('./Commands/').forEach(dir => {
+        const commands = readdirSync(`./Commands/${dir}/`).filter(file => file.endsWith('.js'));
+        for(let file of commands){
+            let pull = require(`../Commands/${dir}/${file}`);
+            if(pull.name){
+                client.commands.set(pull.name, pull);
+                table.addRow(file,'✔️ -> Command Loaded')
+            } else {
+                table.addRow(file, '❌ -> missing a help.name, or help.name is not a string.')
+                continue;
+            }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+        }
+    });
+    
+    console.log(table.toString());
 }
