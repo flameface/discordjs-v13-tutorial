fd7c5ce4d7e83253ea40b77834ecadb010cebe82
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-22 16:07:56 +0530

    Update Slash.js

--- 20-Slash Command Handler/Structures/Slash.js
+++ 20-Slash Command Handler/Structures/Slash.js
@@ -1,26 +1,33 @@
+let slash = []
 const { glob } = require("glob");
 const { promisify } = require("util");
-const { Client } = require("discord.js");
 const globPromise = promisify(glob);
-
-module.exports = async (client) => {
-    client.logger.warn("=-=-=-=-=-=-=- Loading slash command(s) -=-=-=-=-=-=-=");
-    const slashCommands = await globPromise(
+const { readdirSync } = require("fs");
+const ascii = require("ascii-table");
+let table = new ascii("Slash commands");
+module.exports = (client) => {
+    client.logger.ready("=-=-=-=-=-=-=- Loading slash command(s) -=-=-=-=-=-=-=");
+    readdirSync("./SlashCommands/").forEach(dir => {
+    const commands = await globPromise(
         `${process.cwd()}/SlashCommands/*/*.js`
     );
-    const arrayOfSlashCommands = [];
-    slashCommands.map((value) => {
-        const file = require(value);
-        if (!file?.name) return;
-        client.slashCommands.set(file.name, file);
-        client.logger.log(`Loading Slash Command: ${file.name}`)
-
-
-        if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
-        arrayOfSlashCommands.push(file);
+        for (let file of commands) {
+            let pull = require(`../SlashCommands/${dir}/${file}`);
+    
+            if (pull.name) {
+                client.slash.set(pull.name, pull);
+                slash.push(pull);
+                table.addRow(file, '✅ -> Loaded Slash Command');
+            } else {
+                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
+                continue;
+            }
+    
+            }
     });
-    client.on("ready", async () => {
- await client.guilds.cache.get('guild-id').commands.set(arrayOfSlashCommands);
- client.guilds.cache.get('guild-id').commands.set(arrayOfSlashCommands);
-    });
-}; 
+    console.log(table.toString());
+
+client.on("ready",async ()=> {
+    await client.guilds.cache.get('guild-id').commands.set(slash);
+ })
+}
