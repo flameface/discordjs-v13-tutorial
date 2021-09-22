c24b399b6715fdc187ac4f216a9eb5947857c06b
FlameQuard <84311327+FlameQuard@users.noreply.github.com>
2021-09-22 16:38:38 +0530

    Update Slash.js

--- 20-Slash Command Handler/Structures/Slash.js
+++ 20-Slash Command Handler/Structures/Slash.js
@@ -1,23 +1,19 @@
 let slash = []
-const { glob } = require("glob");
-const { promisify } = require("util");
-const globPromise = promisify(glob);
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
+
 let table = new ascii("Slash commands");
-module.exports = async(client) => {
-    client.logger.ready("=-=-=-=-=-=-=- Loading slash command(s) -=-=-=-=-=-=-=");
+module.exports = (client) => {
     readdirSync("./SlashCommands/").forEach(dir => {
-    const commands = await globPromise(
-        `${process.cwd()}/SlashCommands/*/*.js`
-    );
+        const commands = readdirSync(`./SlashCommands/${dir}/`).filter(file => file.endsWith(".js"));
+    
         for (let file of commands) {
             let pull = require(`../SlashCommands/${dir}/${file}`);
     
             if (pull.name) {
                 client.slash.set(pull.name, pull);
                 slash.push(pull);
-                table.addRow(file, '✅ -> Loaded Slash Command');
+                table.addRow(file, '✅');
             } else {
                 table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
                 continue;
@@ -26,8 +22,7 @@ module.exports = async(client) => {
             }
     });
     console.log(table.toString());
-
 client.on("ready",async ()=> {
-    await client.guilds.cache.get('guild-id').commands.set(slash);
+    await clent.guilds.cache.get('guild-id').commands.set(slash);
  })
 }
